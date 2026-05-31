d82e7da2c15e7d6db7d954c33b59a8f63b81bb38
Tallboyyy <adoteybernard15@gmail.com>
2026-05-31 14:26:30 +0000

    Upload assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Bernard Adotey Kpakpo
+# STUDENT ID:  2425403899
+# Email: adoteybernard15@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Bernard Adotey Kpakpo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+  print("print out my initials\n\n\n")
 
+  print("...BAK\n\n\n")
 
+  print("...BBBBBBBBBBBB...  ....AAAAAAA.........KKKKK.....KKK")
+  print("...BBBBBBBBBBBB...  ...AAAAAAAAA........KKKKK....KKK.")
+  print("...BBBB....BBBB...  ..AAA.....AAA.......KKKKK...KKK..")
+  print("...BBBB....BBBB...  ..AAA.....AAA.......KKKKK..KKK...")
+  print("...BBBBBBBBBBBB...  ..AAAAAAAAAAA.......KKKKKKKKK....")
+  print("...BBBBBBBBBBBB...  ..AAAAAAAAAAA.......KKKKKKKK.....")
+  print("...BBBB....BBBB...  ..AAA.....AAA.......KKKKK.KKK....")
+  print("...BBBB....BBBB...  ..AAA.....AAA.......KKKKK..KKK...")
+  print("...BBBBBBBBBBBB..**...AAA.....AAA...**..KKKKK...KKK..")
+  print("...BBBBBBBBBBBB..**...AAA.....AAA...**..KKKKK....KKK.**”)
 
 main()
